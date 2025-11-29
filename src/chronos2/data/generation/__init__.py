3f5e46002609109467debd83f2364b2d753bc695
Emanuele Romito <e.romito3@studenti.uniba.it>
2025-11-29 10:56:47 +0100

    feat: refactored data generation and training data

--- src/chronos2/data/generation/__init__.py
+++
@@ -1,4 +0,0 @@
-from .base import TimeSeriesGenerator
-from .univariate import KernelSynthGenerator, ARGenerator, TrendSeasonalityGenerator
-from .multivariate import Multivariatizer, LinearMixupMultivariatizer, NonLinearMixupMultivariatizer, SequentialMultivariatizer
-from .tasks import TaskSampler
